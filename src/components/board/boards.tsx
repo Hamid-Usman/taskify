f7f10fb39082df652750d26382e14f57d3a13570
hamidusman <abdulhamidusman218@gmail.com>
2024-11-29 23:50:55 +0100

    minor adjustments to previous push

--- src/components/board/boards.tsx
+++ src/components/board/boards.tsx
@@ -11,12 +11,13 @@ export const UserBoards = () => {
 
             <div className="flex flex-wrap justify-between lg:justify-normal lg:gap-3">
                 <article className="w-[48%] lg:w-[220px] h-[80px] mt-2 p-1
-                    bg-primary hover:bg-primary_low text-secondary font-bold rounded-md">
+                    bg-primary hover:bg-primary_low text-secondary font-bold rounded-md
+                    text-[14px] md:text-lg ">
                     <h1>Avanti</h1>
                 </article>
                 <article className="w-[48%] lg:w-[220px] h-[80px] mt-2
                     bg-accent_low text-accent font-bold rounded-md
-                    flex justify-center items-center">
+                    flex justify-center items-center text-[14px] md:text-lg">
                     <h1 className="">Create a new board</h1>
                 </article>
             </div>
