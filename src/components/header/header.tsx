73486394466505adf76424d6df76e1d73dae232b
hamidusman <abdulhamidusman218@gmail.com>
2024-11-30 15:08:50 +0100

    removed srollbar

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -9,7 +9,7 @@ export const Header = () => {
     return (
         <nav
         className="px-3 py-2 md:py-1 fixed w-[100vw] border-b-[1px] border-accent_low
-                flex justify-between items-center"
+                flex justify-between items-center z-50"
         >
         <div className="flex items-center gap-2">
             <div className="p-2 bg-primary_low transition duration-300
