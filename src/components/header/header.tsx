89b6ec1b0ae8ead5f06f0063a9e829589f3a1ff3
hamidusman <abdulhamidusman218@gmail.com>
2024-12-01 08:37:08 +0100

    #2

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -8,7 +8,7 @@ export const Header = () => {
 
     return (
         <nav
-        className="px-3 py-2 md:py-1 top-0 fixed w-screen max-w-full border-b border-accent_low
+        className="px-3 py-2 md:py-1 top-0 fixed w-[100vw] border-b border-accent_low
             flex justify-between items-center z-50"
         >
         <div className="flex items-center gap-2">
