322ccf42d8cb0f416e4865f07c2fd52344806dfa
hamidusman <abdulhamidusman218@gmail.com>
2024-12-03 13:13:06 +0100

    modal for creating boards

--- src/components/board/boards.tsx
+++ src/components/board/boards.tsx
@@ -1,6 +1,13 @@
+import { useState } from "react"
 import { FaUser } from "react-icons/fa6"
+import { BoardModal, SpaceProp } from "../../modals/boardModal"
 
 export const UserBoards = () => {
+    const [modalOpen, setModalOpen] = useState(false)
+    const [space, setSpace] = useState<SpaceProp | null>(null)
+
+    const openModal = () => setModalOpen(true)
+    const closeModal = () => setModalOpen(false)
     return (
         <section className="mt-5 mx-7 border-accent_low border-t-[1px]">
             <div className="flex items-center pt-5 px-2">
@@ -18,8 +25,13 @@ export const UserBoards = () => {
                 <article className="w-[48%] lg:w-[220px] h-[80px] mt-2
                     bg-accent_low text-accent font-bold rounded-md
                     flex justify-center items-center text-[14px] md:text-lg">
-                    <h1 className="">Create a new board</h1>
+                    <h1 className=""
+                    onClick={openModal}>Create a new board</h1>
                 </article>
+                {modalOpen && (
+                <BoardModal 
+                closeModal={closeModal}
+                setSpace={setSpace}/>)}
             </div>
         </section>
     )
