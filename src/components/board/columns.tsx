22816689dfd930b2f57aea88c5fb5328d897d2f3
hamidusman <abdulhamidusman218@gmail.com>
2024-12-04 13:15:17 +0100

    adjustments to cards and home page

--- src/components/board/columns.tsx
+++ src/components/board/columns.tsx
@@ -48,7 +48,7 @@ export const Column = ({
         className={`min-h-fit bg-[#010B13] p-3 rounded-md w-[240px] transition-colors`}
         >
             {filteredCards.map((card) =>{
-                return <Card key={card.id} {...card}/>;
+                return <Card key={card.title} {...card}/>;
             })}
         <DropIndicator beforeId={null} column={column} />
         <AddCard column={column} setCards={setCards} />
