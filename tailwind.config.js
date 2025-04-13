8e4e40dd0eca11f11eccea8163e0f7f1ac42a58a
Hamid-Usman <abdulhamidusman218@gmail.com>
2025-04-13 11:52:05 +0100

    worked on function to render tasks based on priorities

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,19 +7,21 @@ export default {
     "./src/**/*.{js,jsx,ts,tsx}"
 	],
 	theme: {
-	colors: {
-		primary: '#F28F3B',
-		accent: '#9DB4C0',
-		accent_low: '#3C484E',
-		secondary: '#1B2228',
-		secondary_low: '#9db4c027',
-		white: '#F7F7F7',
-		gray: '#545454',
-		primary_low: '#f2903b1e',
-		black: '#010B13',
-		error: '#DC143C',
-		error_subtle: '#f2738e'
-	},
+		colors: {
+			primary: '#F28F3B',
+			accent: '#9DB4C0',
+			accent_low: '#3C484E',
+			secondary: '#1B2228',
+			secondary_low: '#9db4c027',
+			white: '#F7F7F7',
+			gray: '#545454',
+			primary_low: '#f2903b1e',
+			black: '#010B13',
+			error: '#DC143C',
+			error_subtle: '#f2738e'
+
+			
+		},
 	extend: {
 		borderRadius: {
 			lg: 'var(--radius)',
